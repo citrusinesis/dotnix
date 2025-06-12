591849242a8083e8a601fe2ccca32ea8a5cab465
Jiho Song <jihojiho2003@gmail.com>
2025-06-12 09:40:12 +0900

    update vscode configuration with enhanced settings and additional extensions

--- home/citrus/programs/vscode.nix
+++ home/citrus/programs/vscode.nix
@@ -2,6 +2,65 @@
 {
   programs.vscode = {
     enable = true;
+
+    settings = {
+      editor.formatOnSaveMode = "modificationsIfAvailable";
+      editor.formatOnType = true;
+
+      editor.smoothScrolling = true;
+      editor.cursorSmoothCaretAnimation = "on";
+      editor.cursorBlinking = "smooth";
+      workbench.list.smoothScrolling = true;
+      terminal.integrated.smoothScrolling = true;
+
+      editor.fontFamily = "GeistMono NF, D2CodingLigature Nerd Font, monospace";
+      editor.fontSize = 15;
+      terminal.integrated.fontSize = 14;
+
+      editor.formatOnPaste = true;
+      editor.formatOnSave = true;
+
+      terminal.integrated.enableMultiLinePasteWarning = "never";
+
+      workbench.iconTheme = "catppuccin-mocha";
+      workbench.colorTheme = "Catppuccin Mocha";
+
+      workbench.sideBar.location = "right";
+      workbench.activityBar.location = "top";
+
+      git.autofetch = true;
+
+      nix.serverPath = "nil";
+      nix.serverSettings = {
+        nil = {
+          formatting = {
+            command = [ "nixfmt" ];
+          };
+          nix = {
+            flake = {
+              nixpkgsInputName = "nixpkgs";
+            };
+          };
+        };
+      };
+      nix.enableLanguageServer = true;
+      nixEnvSelector.useFlakes = true;
+
+      javascript.format.enable = false;
+      typescript.format.enable = false;
+      css.format.enable = false;
+      less.format.enable = false;
+      scss.format.enable = false;
+      html.format.enable = false;
+      json.format.enable = false;
+
+      github.copilot.enable = {
+        "*" = true;
+        plaintext = false;
+        markdown = false;
+        scminput = false;
+      };
+    };
     
     profiles.default.extensions = with pkgs.vscode-extensions; [
       catppuccin.catppuccin-vsc-icons
@@ -16,6 +75,12 @@
       github.vscode-pull-request-github
       github.vscode-github-actions
       github.copilot
+
+      usernamehw.errorlens
+      gruntfuggly.todo-tree
+
+      donjayamanne.githistory
+      eamodio.gitlens
     ];
   };
 }
