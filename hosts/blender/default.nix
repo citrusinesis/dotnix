56f9f66fe23bc7a0fa1cf6d1dd7b09f47ad1cbaa
Jiho Song <jihojiho2003@gmail.com>
2025-08-25 00:32:20 +0900

    fix(audio): Disable node suspension in WirePlumber to fix audio delay/pop during playback

--- hosts/blender/default.nix
+++ hosts/blender/default.nix
@@ -62,6 +62,51 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    
+    # Fix audio delay/pop when starting playback by disabling node suspension
+    wireplumber.configPackages = [
+      (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/disable-suspension.conf" ''
+        monitor.alsa.rules = [
+          {
+            matches = [
+              {
+                # Matches all sources
+                node.name = "~alsa_input.*"
+              },
+              {
+                # Matches all sinks
+                node.name = "~alsa_output.*"
+              }
+            ]
+            actions = {
+              update-props = {
+                session.suspend-timeout-seconds = 0
+              }
+            }
+          }
+        ]
+        # bluetooth devices
+        monitor.bluez.rules = [
+          {
+            matches = [
+              {
+                # Matches all sources
+                node.name = "~bluez_input.*"
+              },
+              {
+                # Matches all sinks
+                node.name = "~bluez_output.*"
+              }
+            ]
+            actions = {
+              update-props = {
+                session.suspend-timeout-seconds = 0
+              }
+            }
+          }
+        ]
+      '')
+    ];
   };
 
   # Bluetooth
